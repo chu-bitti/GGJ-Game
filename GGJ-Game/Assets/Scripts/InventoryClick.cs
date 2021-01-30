225e865b46345908bdedf8a3ca19e7c46ee08656
chu-bitti <72223949+chu-bitti@users.noreply.github.com>
2021-01-30 13:56:53 -0500

    Inventory and PickUp

---
+++ GGJ-Game/Assets/Scripts/InventoryClick.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InventoryClick : MonoBehaviour
+{
+    private bool isPaused;
+    public GameObject inventoryPanel;
+    public GameObject InventoryButton;
+
+    void Start() {
+      isPaused = false;
+      inventoryPanel.SetActive(false);
+    }
+
+    public void Update() {
+    }
+
+    public void Pause() {
+        isPaused = !isPaused;
+        if(isPaused) {
+          inventoryPanel.SetActive(true);
+          Time.timeScale = 0f;
+        } else {
+          inventoryPanel.SetActive(false);
+          Time.timeScale = 1f;
+        }
+      }
+
+    public void Resume() {
+      isPaused = !isPaused;
+    }
+}
--- GGJ-Game/Assets/Scripts/InventoryItem.cs
+++ GGJ-Game/Assets/Scripts/InventoryItem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Items")]
 public class InventoryItem : ScriptableObject
 {
@@ -10,4 +11,16 @@ public class InventoryItem : ScriptableObject
     public int numberHeld;
     public bool usable;
     public bool unique;
+    public UnityEvent thisEvent;
+
+    public void Use() {
+      thisEvent.Invoke();
+    }
+
+    public void DecreaseAmount(int amountToDecrease) {
+      numberHeld-= amountToDecrease;
+      if (numberHeld < 0) {
+        numberHeld = 0;
+      }
+    }
 }
