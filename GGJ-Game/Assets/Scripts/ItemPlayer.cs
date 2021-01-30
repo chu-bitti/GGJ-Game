08554f6b450567a80c5e65734fe5af8466eeb2a8
chu <churushii@gmail.com>
2021-01-29 19:50:30 -0500

    Oof

---
+++ GGJ-Game/Assets/Scripts/ItemPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPlayer : MonoBehaviour
+{
+  private Transform player;
+  public bool collision;
+
+  void Start() {
+    collision = false;
+    player = GameObject.FindGameObjectWithTag("Player").transform;
+  }
+
+  public void OnTriggerEnter2D(Collider2D other)
+  {
+    if(other.tag == "Circle")
+    {
+    collision = true;
+    }
+  }
+
+  public void OnTriggerExit2D(Collider2D other)
+  {
+    if(other.tag == "Circle")
+    {
+    collision = false;
+    }
+  }
+
+}
---
+++ GGJ-Game/Assets/Scripts/Ritual.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ritual : MonoBehaviour
+{
+  public GameObject candle;
+
+  public void OnTriggerEnter2D(Collider2D other)
+  {
+    if(other.tag == "Circle")
+    {
+      Destroy(candle);
+    }
+  }
+}
