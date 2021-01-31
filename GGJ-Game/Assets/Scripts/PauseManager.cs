2598b0770681d87835a7731307ffb639788f652d
chu-bitti <72223949+chu-bitti@users.noreply.github.com>
2021-01-31 02:50:44 -0500

    Start & Pause Menu

---
+++ GGJ-Game/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    private bool isPaused;
+    public GameObject pausePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel")) {
+            Resume();
+        }
+    }
+
+    public void Resume() {
+      isPaused = !isPaused;
+      if(isPaused) {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+      } else {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+      }
+    }
+
+    public void QuitToDesktop() {
+      Application.Quit();
+    }
+}
